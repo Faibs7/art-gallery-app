45c9697652a84ed6b1759783c993d69bcb6eca17
FabianMrkwitz <fmrkwitz@gmail.com>
2023-07-31 14:55:27 +0200

    fetch data via swr

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,11 @@
+import useSWR from "swr";
+
 export default function HomePage() {
+  const { data, isLoading, error } = useSWR(
+    "https://example-apis.vercel.app/api/art"
+  );
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
   return (
     <div>
       <h1>Hello from Next.js</h1>
